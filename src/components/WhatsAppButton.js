91e7ef218c2c8c1e430d0d268b941132d7221b1f
Gilang <gilang@itsupport.com>
2026-08-19 15:02:53 +0800

    style: replace headset icon with customer service person icon

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -10,8 +10,10 @@ const contacts = [
 
 const HelpdeskIcon = ({ className }) => (
   <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M3 18v-6a9 9 0 0 1 18 0v6" />
-    <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z" />
+    <circle cx="12" cy="8" r="4" />
+    <path d="M6 21v-2a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v2" />
+    <path d="M18 11a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2" />
+    <path d="M15 11v-1a3 3 0 0 1 6 0v1" />
   </svg>
 );
 
